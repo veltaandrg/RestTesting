774d20b8130a3038dcfad92554b4d01e0d5db0e6
Rodion Gorkovenko <RodionGork@github.com>
2015-05-13 14:57:55 +0300

    Random test for adding to basket

--- src/test/java/restdemo/AppTest.java
+++ src/test/java/restdemo/AppTest.java
@@ -2,6 +2,7 @@ package restdemo;
 
 import java.util.*;
 import org.junit.*;
+import org.json.*;
 import com.mashape.unirest.http.*;
 
 public class AppTest {
@@ -26,4 +27,19 @@ public class AppTest {
     public void testPutDeleteCustomers() throws Exception {
         // create test for adding a customer and then deleting it
     }
+
+    @Test
+    public void testBasket() throws Exception {
+        Random rnd = new Random();
+        JSONArray customers = Unirest.get(siteUrl + customersUrl).asJson().getBody().getObject().getJSONArray("data");
+        JSONArray goods = Unirest.get(siteUrl + "/goods/").asJson().getBody().getObject().getJSONArray("data");
+        int quantity = rnd.nextInt(5) + 1;
+        int customerId = customers.getJSONObject(rnd.nextInt(customers.length())).getInt("id");
+        int productId = goods.getJSONObject(rnd.nextInt(goods.length())).getInt("id");
+        HttpResponse<JsonNode> result = Unirest.post(siteUrl + "/baskets/" + customerId)
+                .body("{\"productid\":" + productId + ",\"quantity\":" + quantity + "}").asJson();
+        Assert.assertEquals(200, result.getStatus());
+        System.out.println(String.format("Put to Basket: %d items of Product #%d for Customer #%d", quantity, productId, customerId));
+    }
+
 }
